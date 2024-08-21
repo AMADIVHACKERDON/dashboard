ac1485528b6f9147057c3979d506b890b1b8b0d8
AMADIVHACKERDON <101997952+AMADIVHACKERDON@users.noreply.github.com>
2024-08-21 03:54:08 +0100

    Update dash.js

--- dash.js
+++ dash.js
@@ -1,18 +1,24 @@
-const project = document.querySelectorAll('.main')
+const project = document.querySelectorAll('.Embedded_Device_Signals')
 
 const displayProject = (values) =>{
     project.forEach(element =>{
         element.style.display ="none"
-        //const name = element.children[1].innerHTML.toUpperCase();
-        //const position = element.children[2].innerHTML.toUpperCase();
-        //const job_type = element.children[3].innerHTML.toUpperCase();
-
-        //if (name.includes(values) || position.includes(values) || job_type.includes(values)){
-          //  element.style.display = "block"
-        //}
+        const title = element.querySelector('h2').innerHTML.toUpperCase();
+        const status = element.dataset.status.toUpperCase();
+        const recent = element.querySelector('ul').innerHTML.toUpperCase();
+        if (title.includes(values)){
+            element.style.display = "block"
+        }
+        else if (values != 'RECENT' && status.includes(values)){
+            element.style.display = "block"
+        }
     })
 }
 
 searchDevice.addEventListener('input', (e) =>{
+    displayProject(e.target.value.toUpperCase())
+})
+
+projectsFilter.addEventListener('input', (e) =>{
     displayProject(e.target.value.toUpperCase())
 })
